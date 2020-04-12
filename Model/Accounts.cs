f0fb486320a735a347bdf5b02c68ec4886b23f10
Jim Stanley <jim.stanley9@gmail.com>
2020-04-12 08:21:25 -0400

    Add project files.

---
+++ Model/Accounts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaltyLogistics.Model
+{
+    class Accounts
+    {
+        public bool IsActive { get; set; }
+        public string Account { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
